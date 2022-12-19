b3bbacd914c7e7dc8e8d01fa43468497459de541
Hareekshith <83201177+Hareekshith@users.noreply.github.com>
2022-12-19 21:08:38 +0530

    Update wincheck.py

--- modules/wincheck.py
+++ modules/wincheck.py
@@ -1,45 +1,39 @@
+import random
 from modules.placevalues import top, mid, bot
-# from main import usermode, pcmode
 
 win = [0]
+usermode = random.choice(["X", "O"])
+pcmode = "X" if usermode == "O" else "O"
 
-# def usorpc(person):
-#     if person == usermode:
-#         return print("You won")
-#     elif person == pcmode:
-#         return print("PC wins")
+def usorpc(person):
+    if person == usermode:
+        print("You won the game")
+    elif person == pcmode:
+        print("The computer won the game")
 
 def checklinematch(person):
     global win
     if top[0] == top[1] == top[2] == person:
-        # usorpc(person)
-        print(f"{person} wins!")
+        usorpc(person)
         return win.append(1)
     elif mid[0] == mid[1] == mid[2] == person:
-        # usorpc(person)
-        print(f"{person} wins!")
+        usorpc(person)
         return win.append(1)
     elif bot[0] == bot[1] == bot[2] == person:
-        # usorpc(person)
-        print(f"{person} wins!")
+        usorpc(person)
         return win.append(1)
     elif top[0] == mid[0] == bot[0] == person:
-        # usorpc(person)
-        print(f"{person} wins!")
+        usorpc(person)
         return win.append(1)
     elif top[1] == mid[1] == bot[1] == person:
-        # usorpc(person)
-        print(f"{person} wins!")
+        usorpc(person)
         return win.append(1)
     elif top[2] == mid[2] == bot[2] == person:
-        # usorpc(person)
-        print(f"{person} wins!")
+        usorpc(person)
         return win.append(1)
     elif top[0] == mid[1] == bot[2] == person:
-        # usorpc(person)
-        print(f"{person} wins!")
+        usorpc(person)
         return win.append(1)
     elif top[2] == mid[1] == bot[0] == person:
-        # usorpc(person)
-        print(f"{person} wins!")
+        usorpc(person)
         return win.append(1)
